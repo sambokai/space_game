bc5b0a6404a1100b071fb7b4d5880b13d3899a7c
Sam Bokai <sam.bokai@student.HTW-Berlin.de>
2017-01-05 16:55:56 +0100

    Add: look behind toggle (‘R’) for player camera

---
+++ Assets/PlayerShip/Camera/CameraControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour {
+
+	private Transform tx;
+
+	// Use this for initialization
+	void Start () {
+		tx = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.R)) {
+			tx.Rotate(new Vector3(0,180,0));
+		}
+	}
+
+	void rotateCamera(Vector3 rot) {
+	}
+}
